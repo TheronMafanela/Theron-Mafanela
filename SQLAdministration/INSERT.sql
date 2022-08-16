3b150d450d336e16f252731e3ec2ecf307622c3a
Theron Mafanela <theron.mafanela@sambeconsulting.com>
2022-08-16 08:38:24 +0200

    Added a file with select SQL Queries to insert data in a table

---
+++ SQLAdministration/INSERT.sql
@@ -0,0 +1,16 @@
+USE [OurFirstDatabase]
+GO
+
+INSERT INTO [dbo].[personalinfo]
+           ([FirstName]
+           ,[LastName]
+           ,[dob]
+           ,[ID])
+     VALUES
+           ('Theron'
+           ,'Mafanela'
+           ,'01/06/1998'
+           ,980601)
+GO
+
+
