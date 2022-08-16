49c2643e3a1bcffa30d8c58d66340e6c435ba8e3
Theron Mafanela <theron.mafanela@sambeconsulting.com>
2022-08-16 16:14:12 +0200

    adding an alternative way of creating a temp table in mssql

--- SQLAdministration/TempTable.sql
+++ SQLAdministration/TempTable.sql
@@ -5,3 +5,16 @@ WHERE Title = 'mr.'
 
 
 SELECT * FROM #TempPersonTable
+
+DROP TABLE #TempPersonTable
+
+CREATE TABLE #TempPersonTable(
+BusinessEntityId int,
+FirstName varchar(50),
+LastName varchar(50),
+Title nvarchar(50)
+)
+INSERT INTO #TempPersonTable
+SELECT BusinessEntityID,FirstName,LastName,Title
+FROM [Person].[Person]
+WHERE Title = 'mr.'
